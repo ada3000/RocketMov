e08e7c7ed30f04d86f6fde52b08e9c1ebbad401a
ada <ada3000@yandex.ru>
2021-01-10 01:12:42 +0300

    VideoConverter Step 1

---
+++ RocketMov/frmMain.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace RocketMov
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader Name;
+            System.Windows.Forms.ColumnHeader Size;
+            System.Windows.Forms.ColumnHeader Status;
+            System.Windows.Forms.ColumnHeader Id;
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.cbConvert = new System.Windows.Forms.Button();
+            Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            Id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            Name.Text = "Name";
+            Name.Width = 180;
+            // 
+            // Size
+            // 
+            Size.Text = "Size";
+            Size.Width = 80;
+            // 
+            // Status
+            // 
+            Status.Text = "Status";
+            Status.Width = 100;
+            // 
+            // Id
+            // 
+            Id.Text = "Id";
+            Id.Width = 40;
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lvFiles.AllowDrop = true;
+            this.lvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            Id,
+            Name,
+            Size,
+            Status});
+            this.lvFiles.FullRowSelect = true;
+            this.lvFiles.GridLines = true;
+            this.lvFiles.HideSelection = false;
+            this.lvFiles.Location = new System.Drawing.Point(0, 0);
+            this.lvFiles.MultiSelect = false;
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(412, 554);
+            this.lvFiles.TabIndex = 0;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            this.lvFiles.SelectedIndexChanged += new System.EventHandler(this.lvFiles_SelectedIndexChanged);
+            this.lvFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.lvFiles_DragDrop);
+            this.lvFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.lvFiles_DragEnter);
+            // 
+            // cbConvert
+            // 
+            this.cbConvert.Location = new System.Drawing.Point(608, 12);
+            this.cbConvert.Name = "cbConvert";
+            this.cbConvert.Size = new System.Drawing.Size(75, 23);
+            this.cbConvert.TabIndex = 1;
+            this.cbConvert.Text = "Convert";
+            this.cbConvert.UseVisualStyleBackColor = true;
+            this.cbConvert.Click += new System.EventHandler(this.cbConvert_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Controls.Add(this.cbConvert);
+            this.Controls.Add(this.lvFiles);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.Button cbConvert;
+    }
+}
---
+++ RocketMov/frmMain.cs
@@ -0,0 +1,229 @@
+﻿using FFMpegCore;
+using FFMpegCore.Enums;
+using MediaToolkit;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RocketMov
+{
+    public partial class frmMain : Form
+    {
+        private List<FileDesc> _files = new List<FileDesc>();
+        int fileId = 1;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //lvFiles.Items.Add(new ListViewItem(new[] { "0", "1", "2", "3" }));
+            //AddFile(@"F:\Pictures\Tmp\New\2019\(09) LenaPhone\Video\2019-09-02 11-44-03.MOV");
+        }
+
+        private void lvFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(1);
+            //lvFiles.SelectedItems[0];
+        }
+
+        private void lvFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                AddFile(file);
+            }
+        }
+
+        private void lvFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void AddFile(string path)
+        {
+            var file = new FileDesc
+            {
+                Id = fileId++,
+                PathIn = path,
+                PathOut = GetPathOut(path),
+                Name = System.IO.Path.GetFileName(path),
+                Size = new FileInfo(path).Length,
+                Status = FileStatus.Wait
+            };
+
+            _files.Add(file);
+
+            var viewDesc = GetViewDesc(file);
+
+            AddOrUpdateFileDesc(viewDesc);
+        }
+
+        private void AddOrUpdateFileDesc(FileViewDesc viewDesc)
+        {
+            if (InvokeRequired)
+            {
+                Action<FileViewDesc> d = new Action<FileViewDesc>(AddOrUpdateFileDesc);
+                Invoke(d, viewDesc);
+                return;
+            }
+
+            for (int i = 0; i < lvFiles.Items.Count; i++)
+            {
+                var lvi = lvFiles.Items[i] as ListViewItem;
+                if (lvi.Text == viewDesc.Id)
+                {
+                    lvFiles.Items[i] = viewDesc.ToListViewItem();
+                    return;
+                }
+            }
+
+            lvFiles.Items.Add(viewDesc.ToListViewItem());
+        }
+
+        private FileViewDesc GetViewDesc(FileDesc file)
+        {
+            return new FileViewDesc
+            {
+                Id = file.Id.ToString(),
+                Name = file.Name,
+                Size = file.Size / 1024 + " KB",
+                Status = file.Status.ToString()
+            };
+        }
+
+        private string GetPathOut(string path)
+        {
+            int dotIdx = path.LastIndexOf('.');
+            path = path.Substring(0, dotIdx) + "_c.mp4";
+
+            return path;
+        }
+
+        private void cbConvert_Click(object sender, EventArgs e)
+        {
+            cbConvert.Enabled = false;
+            var files = _files.ToArray();
+
+            foreach (var file in files)
+            {
+                if (file.Status == FileStatus.Wait)
+                {
+                    ConvertFile(file);
+                }
+            }
+
+            cbConvert.Enabled = true;
+        }
+
+
+
+
+        private void ConvertFile(FileDesc file)
+        {
+            file.Status = FileStatus.Converting;
+            AddOrUpdateFileDesc(GetViewDesc(file));
+
+            //ConverterV1(file);
+            ConverterV2(file);
+
+            file.Status = FileStatus.Converted;
+            AddOrUpdateFileDesc(GetViewDesc(file));
+        }
+
+        private void ConverterV1(FileDesc file)
+        {
+            var conversionOptions = new ConversionOptions
+            {
+                //MaxVideoDuration = TimeSpan.FromSeconds(30),
+                //VideoAspectRatio = VideoAspectRatio.R16_9,
+                CustomHeight = 1280,
+                CustomWidth = 720,
+                VideoSize = MediaToolkit.Options.VideoSize.Custom,
+                VideoBitRate = 4 * 1024
+            };
+
+            using (var engine = new Engine(@"F:\Pictures\Tmp\Converter\ffmpeg.exe"))
+            {
+                engine.CustomCommand("-threads 4");
+                engine.ConvertProgressEvent += Engine_ConvertProgressEvent;
+                engine.ConversionCompleteEvent += Engine_ConversionCompleteEvent;
+                //engine.f
+                engine.Convert(new MediaFile(file.PathIn), new MediaFile(file.PathOut), conversionOptions);
+            }
+        }
+
+        private void ConverterV2(FileDesc file)
+        {
+            FFMpegOptions.Configure(new FFMpegOptions { RootDirectory = @"D:\Git\RocketMov\RocketMov\ffmpeg" });
+
+            FFMpegArguments
+                .FromFileInput(file.PathIn)
+                .OutputToFile(file.PathOut, true, options => options
+                    .WithVideoCodec(VideoCodec.LibX264)
+                    .WithConstantRateFactor(21)
+                    .WithAudioCodec(AudioCodec.Aac)
+                    .WithVariableBitrate(4)
+                    .WithFastStart()
+                    .Scale(720, 1280)
+                    .UsingThreads(4))
+                .ProcessSynchronously();
+        }
+
+        private void Engine_ConversionCompleteEvent(object sender, ConversionCompleteEventArgs e)
+        {
+            //pbConvert.Value = 100;
+        }
+
+        private void Engine_ConvertProgressEvent(object sender, ConvertProgressEventArgs e)
+        {
+            //pbConvert.Value - e.
+        }
+    }
+
+    public class FileViewDesc
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Status { get; set; }
+
+        public ListViewItem ToListViewItem()
+        {
+            return new ListViewItem(new[] { Id.ToString(), Name, Size, Status });
+        }
+    }
+
+    public class FileDesc
+    {
+        public int Id { get; set; }
+        public string PathIn { get; set; }
+        public string PathOut { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public FileStatus Status { get; set; }
+    }
+
+    public enum FileStatus
+    {
+        Wait,
+        Fail,
+        Converting,
+        Converted
+    }
+}
